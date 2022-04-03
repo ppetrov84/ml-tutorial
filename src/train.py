e7e74c925e7995c0e37080ea77b61ff4e7902243
ppetrov84 <petar.m.petrov@gmail.com>
2022-04-03 20:43:32 +0200

    src\train.py

--- src/train.py
+++ src/train.py
@@ -1,24 +1,25 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.lienar_model import LienarRegression
+from sklearn.linear_model import LinearRegression
 import pickle
 
-data=pd.read_csv("data/mpg.csv", sep=";")
+data = pd.read_csv("data/mpg.csv", sep=";")
 
-print (data)
+print(data)
 
-data=data.sample(frac=1)
+data = data.sample(frac=1)
 
-y_variable=data["mpg"]
+y_variable = data["mpg"]
 
 x_variables = data.loc[:, data.columns != "mpg"]
 
-x_train, x_test, y_train, y_test = train_test_split(x_variables, y_variable, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(
+    x_variables, y_variable, test_size=0.2)
 
-regressor=LinearRegression()
+regressor = LinearRegression()
 
-regressor= regressor.fit(x_train, y_train)
-y_pred=regressor.predict(x_test)
+regressor = regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
 
-file_to_write=open("data/models/baummethoden_lr.pickle", "wb")
+file_to_write = open("data/models/baummethoden_lr.pickle", "wb")
 pickle.dump(regressor, file_to_write)
